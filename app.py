71bfef73414ab1104c4ab987c91c92dbb1a79727
teknofage <teknofage@gmail.com>
2019-09-29 20:06:07 -0700

    third commit

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.Playlister
+playlists = db.playlists
 
 app = Flask(__name__)
 
-playlists = [
-    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
-    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
-]
+# playlists = [
+#     {'title': 'Cat Videos', 'description': 'Cats acting weird'},
+#     {'title': '80\'s Music', 'description': 'Don\'t stop believing!'},
+#     {'title': "Freddie Mercury's Outfits", 'description': 'Look at that hairy chest!'}
+# ]
 
-@app.route('/')
-# OUR MOCK ARRAY OF PROJECTS
-@app.route('/')
+
 def playlists_index():
     """Show all playlists."""
     return render_template('playlists_index.html', playlists=playlists)
-def index():
-    """Return homepage."""
-    return render_template('home.html', msg='Flask is Cool!!')
+
+    def index():
+        """Return homepage."""
+        return render_template('home.html', msg='Flask is Cool!!')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
